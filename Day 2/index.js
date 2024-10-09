36f991d300292762000392bcea0549f8bf3f02d2
Pankaj <84912901+Pankajrao55@users.noreply.github.com>
2024-10-09 20:49:12 +0530

    Add files via upload

---
+++ Day 2/index.js
@@ -0,0 +1,126 @@
+console.log("hello world");
+
+// Date : 09/10/24
+
+// OPERATORS
+
+//multiplication
+
+// let a = 5;
+// let b = 10;
+// let res = a*b;
+// console.log(res);
+
+
+// //addition
+
+
+// let a = 5;
+// let b = 10;
+// let res = a+b;
+// console.log(res);
+
+
+
+// //division
+
+// let a = 5;
+// let b = 10;
+// let res = a/b;
+// console.log(res);
+
+
+// //subtraction
+
+
+// let a = 5;
+// let b = 10;
+// let res = a-b;
+// console.log(res);
+
+
+
+//Comparision operator
+
+// ==
+
+// let a = 5;
+// let b = 10;
+// let res = a==b;
+// console.log(res);
+
+//===
+
+// let a = 5;
+// let b = 10;
+// let res = a===b;
+// console.log(res);
+
+
+// !==
+
+// let a = 10;
+// let b = 10;
+// let res = a!==b;
+// console.log(res);
+
+// !=
+
+// let a = "10";
+// let b = 10;
+// let res = a!=b;
+// console.log(res);
+
+// !==
+
+// let a = "10";
+// let b = 10;
+// let res = a!==b;
+// console.log(res);
+
+// >
+
+// let a = "10";
+// let b = 10;
+// let res = a > b;
+// console.log(res);
+
+// < Operator
+
+// let a = 5;
+// let b = 10;
+// let res = a<b;
+// console.log(res);
+
+// >= Operator
+
+// let a = 5;
+// let b = 10;
+// let res = a>=b;
+// console.log(res);
+
+// <= Operator
+
+// let a = 5;
+// let b = 10;
+// let res = a<=b;
+// console.log(res);
+
+
+
+// +=
+
+let a = "10";
+let b = 10;
+b = +5;
+//let res = a%b;
+console.log(b);
+
+// +/
+
+
+// let a = "10";
+// let b = 10;
+// b = -5;
+// //let res = a%b;
+// console.log(b);
