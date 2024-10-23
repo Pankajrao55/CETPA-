2270dd831d4a6b8862e18f5064797cf1142a626d
Pankaj <84912901+Pankajrao55@users.noreply.github.com>
2024-10-23 21:24:10 +0530

    Add files via upload

---
+++ Day 4/index.js
@@ -0,0 +1,47 @@
+// loops 14/10/24
+
+
+// for loop 
+ 
+// const n = 5;
+// for (let i=1; i<=n; i++){
+//     console.log("iam a programmer");
+// }
+
+// Multiple of 3
+
+// const a = 3;
+// for (let i =1; i<=10; i++){
+//     console.log();
+// }
+
+// for(i=1; i<=10; i++){
+//     console.log(`3 X ${i} = ${3*i}`);
+// }
+
+
+
+// for in loop
+
+// const student={
+//     name: 'Pankaj',
+//     class: Graduate,
+//     age: 22
+// }
+
+// for (let key in student){
+//     console.log(`${key} => ${student[key]}`);
+// }
+
+
+// print pattern 
+
+
+for (i =1; i<=8; i++) {
+  let pattern = "";
+    for(let j=1; j<=i; j++){
+        pattern += j + "";
+    }
+    console.log(pattern);
+
+}
