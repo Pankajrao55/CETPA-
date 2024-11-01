4340589d1d7c24c1f8da0cc022a6c43b1bdc0ace
Pankaj <84912901+Pankajrao55@users.noreply.github.com>
2024-11-01 21:11:23 +0530

    Add files via upload

---
+++ Day 6/index.js
@@ -0,0 +1,95 @@
+// ***************************************************************************//
+
+// string Replace method 
+
+// let num = " we are an amazing developer";
+// console.log(num.replace(" ", "/"));
+// console.log(num.replaceAll("amazing","best"));
+// console.log(num.trim());
+
+
+// **********************************************************************//
+// string split method
+
+// let num1 = " we are an amazing developer";
+// console.log(num1.split("are"));
+
+
+// **************************************************//
+
+// string length 
+
+
+// let num2 = " we are an amazing developer";
+// console.log(num2.length);
+
+
+//**************************************************************// 
+
+// Question 
+
+// let num3 = "hi prepbytes";
+// console.log(num3.slice(3));
+
+
+// *************************************************************************//
+
+// Arrays push() method
+
+// const arr = ["jarvis", "the"];
+// arr.push("developer");
+// console.log(arr);
+
+
+// slice in array 
+
+//  const arr = ["jarvis","the","developer"];
+//   const arr2 = arr.slice(1, 2);
+//   console.log(arr2);
+
+
+//   const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// const citrus = fruits.slice(1, 3);
+// console.log(citrus); // Output: ["Orange", "Lemon"]
+
+
+// toString() method in aaray 
+
+// const arr2 = ["Jarvis" , "the" , "developer"];
+// const str = arr2.toString();
+
+// console.log(str);
+
+
+
+// Shift() method in array 
+
+// let arr = ["Jarvis" , "the" , "developer"];
+// arr.shift();
+// console.log(arr);
+
+
+// **********************************************************//
+
+// Map() method in array 
+
+// const arr5 = [1, 2, 3, 4, 5];
+// const arr6 = arr5.map(x => x * 2);
+// console.log(arr6);
+
+
+// *************************************************************************************//
+// pop() method in array 
+
+//  const arr7 = ["Jarvis" , "the" , "developer"];
+//  arr7.pop();
+//  console.log(arr7);
+
+
+
+ // filter() method in array
+
+//  let arr = ["jarvis", "the", "developer"];
+//  let arr2 = arr.filter(word => word.length > 3);
+//  console.log(arr2);
+ 
