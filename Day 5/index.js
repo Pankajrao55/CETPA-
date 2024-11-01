d9221d6de1d773b124a527d3a4219b602454d7bc
Pankaj <84912901+Pankajrao55@users.noreply.github.com>
2024-11-01 21:10:14 +0530

    Add files via upload

---
+++ Day 5/index.js
@@ -0,0 +1,115 @@
+// give an array find out the total no. of odd and even nos.
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// function printOddAndEven(arr) {
+//     let oddNumbers = [];
+//     let evenNumbers = [];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 2 === 0) {
+//             evenNumbers.push(arr[i]);
+//         } else {
+//             oddNumbers.push(arr[i]);
+//         }
+//     }
+
+//     console.log("Even numbers:", evenNumbers);
+//     console.log("Odd numbers:", oddNumbers);
+// }
+
+// // Example usage:
+// //let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// printOddAndEven(arr);
+
+
+// 
+
+    // let arr = [2, 4, 6, 7, 5, 9, 19] 
+    // even = 0; odd = 0;
+    //  for (let i = 0; i < arr.length; i++) { if (arr[i] % 2 == 0) { even++; } else { odd++; } } 
+    //  console.log(even) console.log(odd)
+
+
+
+// *************************//
+
+
+// while loop and Do while loop
+
+// while(condition){
+//     // code 
+// }
+
+
+// let i = 0;
+// let text = "";
+
+// while (i < 10){
+//     text += "the number is", + i;
+//     i++;
+// }
+// console.log(text);
+
+
+//
+
+
+
+//**********************************// 
+
+
+// do while loop 
+
+
+// let j = 1;
+
+// do{
+//     console.log("Hello world");
+//     j++;
+// }while(j < 5);
+
+
+// ******************////
+// more string methods
+
+// let num = "you will be an amazing developer";
+
+// console.log(num.lastIndexOf("developer"));
+// console.log(num.indexOf("developer"));
+// console.log(num.indexOf("will", 10));
+
+
+// ***********************************//
+
+// strings concatenation
+
+// var string1 = "prep";
+// var string2 = "byles";
+// var string3 = string1 + string2;
+// console.log(string3);
+
+
+// concat() method 
+
+
+// var str1 = "prep";
+// var str2 = "byles";
+// var res = str1.concat(str2);
+// console.log(res);
+
+
+// ***************************************///
+// string slicing 
+
+// let abc = "you will be an amazing developer";
+// console.log(abc.slice(7, 13));
+// console.log(abc.slice(7, -13));
+// //console.log(abc.slice(-7));
+// //console.log(abc.slice(-7, 13));
+
+
+
+// **************************************//
+// Substring 
+// let str = "you will bw an amazing developer";
+// console.log(str.substring(7, 13));
