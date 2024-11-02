9cea719bdee31274a4c0fc9c4eb2491c52e0481d
Pankaj <84912901+Pankajrao55@users.noreply.github.com>
2024-11-02 18:54:05 +0530

    Add files via upload

---
+++ Day 7/index.js
@@ -0,0 +1,81 @@
+// *******************************************************************//
+// sum  () method in array 
+
+let arr = [1,2,3,4];
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+console.log(sum); 
+
+// reverse method ()
+let abc = "We are an amazing developer";
+let reverse = abc.split("").reverse().join("");
+console.log(reverse);
+
+// reverse () method in array 
+
+let arr1 = [1,2,3,4,5,6,7];
+let reverseArr = arr1.reverse();
+console.log(reverseArr);
+
+
+// include () method in array
+
+let arr2 = [1,2,3,4,5,6,7];
+let include = arr2.includes(5);
+console.log(include);
+
+
+
+// unshift () method in array
+
+let arr3 = [1,2,3,4,5,6,7];
+let unshift = arr3.unshift(0);
+console.log(arr3);
+
+
+
+// concat () method in array
+
+let arr4 = [1,2,3,4,5,6,7];
+let concat = arr4.concat([8,9,10]);
+console.log(concat);
+
+
+
+// join () method in array 
+
+let arr5 = ["hi","how","are","you"];
+let join = arr5.join("");
+console.log(join);
+
+
+
+// *************************//
+// indexOf () method in array
+
+let arr6 = [1,2,3,4,5,6,7];
+let index = arr6.indexOf(5);
+console.log(index);
+
+
+// lastindexOf () method in array 
+
+let arr7 = [1,2,3,4,5,6,7];
+let lastIndex = arr7.lastIndexOf(3);
+console.log(lastIndex);
+
+// slice () method in array 
+
+let arr8 = [1,2,3,4,5,6,7];
+let slice = arr8.slice(2,5);
+console.log(slice);
+
+
+
+
+
+
+
+
